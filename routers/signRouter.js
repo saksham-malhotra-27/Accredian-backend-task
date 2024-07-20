8633b0eaa26191f04faa669dbf1e517ab24e6b7c
Saksham <saksh.m.malhotra@gmail.com>
2024-07-20 12:15:54 +0530

    added hashing

--- routers/signRouter.js
+++ routers/signRouter.js
@@ -2,7 +2,10 @@ import z from "zod"
 import prisma from "../db/config.js";
 import { Router } from "express"
 import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+
 const router = Router();
+const saltRounds = 10;
 
 const reqLogin = z.object({
   email: z.string().email(),
@@ -13,27 +16,35 @@ router.post('/login', async (req, res)=>{
     try{
       const result = reqLogin.safeParse(req.body)
       if(result.success){
+        //console.log(result.data)
           const user = await prisma.user.findUnique({
             where:{
               email: result.data.email,
-              password: result.data.password,
             },
             select:{
-              email:true,
+              email:true, password: true,
             }
           })
-  
-          if(user){
-          const token = jwt.sign( user.email,process.env.SECRET)
-          return res.status(200).json({msg:"logged in", token, success:true})
+          
+          if(user ){
+            
+          const validPass = await bcrypt.compare(result.data.password, user.password)
+          if(validPass)
+          { const token = jwt.sign( user.email,process.env.SECRET)
+            return res.status(200).json({msg:"logged in", token, success:true})
+           } 
+          else {
+            return res.status(401).json({error:"Invalid password", success:false})
+          }
           }
           else {
             const referCode = await generateUniqueReferCode();
-            console.log(referCode)
+            const hashedPassword = await bcrypt.hash(result.data.password, saltRounds);
+
             const newUser = await prisma.user.create({
               data:{
                 email: result.data.email,
-                password: result.data.password,
+                password: hashedPassword,
                 referCode: referCode
               }
             })
